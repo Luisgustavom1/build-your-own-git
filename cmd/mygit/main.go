f80d6f2c63b36d2f68f60ffa815ca85f68a0dde8
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-06-23 16:59:14 -0300

    a

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 func main() {
@@ -11,10 +12,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	e, err := os.Executable()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	executableDir := path.Dir(e)
+
 	switch command := os.Args[1]; command {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-			if err := os.MkdirAll(dir, 0755); err != nil {
+			if err := os.MkdirAll(path.Join(executableDir, dir), 0755); err != nil {
 				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
 			}
 		}
@@ -24,9 +32,15 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
 		}
 
-		fmt.Println("Initialized git directory")
+		fmt.Println("Initialized empty Git repository in", path.Join(executableDir, ".git"))
 	case "cat-file":
-		fmt.Println("cat-file")
+		if len(os.Args) < 3 {
+			fmt.Fprintf(os.Stderr, "usage: mygit cat-file <object>\n")
+			os.Exit(1)
+		}
+
+		object := os.Args[2]
+		fmt.Println(object)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
